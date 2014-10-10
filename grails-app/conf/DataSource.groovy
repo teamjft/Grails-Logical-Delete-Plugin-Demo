69dabffe394bc5971ff05e1cc306215921d97e96
Gaurav Chauhan <gchauhan@jellyfishtechnologies.com>
2014-10-10 15:14:14 +0530

    Datasource updated

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,15 +29,6 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            driverClassName = "org.postgresql.Driver"
-            dialect = org.hibernate.dialect.PostgreSQLDialect
-
-            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-
-            url = "jdbc:postgresql://"+uri.host+uri.path
-            username = uri.userInfo.split(":")[0]
-            password = uri.userInfo.split(":")[1]
         }
     }
 }
