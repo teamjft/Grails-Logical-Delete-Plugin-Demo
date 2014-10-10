4709a89d346c0239112d0c66dcb88211a294d2f3
Gaurav Chauhan <gchauhan@jellyfishtechnologies.com>
2014-10-10 15:19:23 +0530

    Datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,6 +29,8 @@ environments {
     }
     production {
         dataSource {
+	    dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
 }
